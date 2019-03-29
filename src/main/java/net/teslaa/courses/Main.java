02ead6321eac8c4cb3a4eebd70dfc2b4e4e5a3fa
Amir <Blackworld.2035@gmail.com>
2019-03-29 15:02:04 +0200

    first commit

---
+++ src/main/java/net/teslaa/courses/Main.java
@@ -0,0 +1,13 @@
+package net.teslaa.courses;
+
+import org.apache.log4j.BasicConfigurator;
+
+import static spark.Spark.get;
+
+public class Main {
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+        get("/hello", (req, res) -> "Hello World");
+
+    }
+}
