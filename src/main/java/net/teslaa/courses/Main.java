ba23f0098d9fe14e9d22cc88bebd041ff8171481
Amir <Blackworld.2035@gmail.com>
2019-03-29 15:26:39 +0200

    Up and running with Spark HTML renderer Handlebars

--- src/main/java/net/teslaa/courses/Main.java
+++ src/main/java/net/teslaa/courses/Main.java
@@ -1,13 +1,17 @@
 package net.teslaa.courses;
 
 import org.apache.log4j.BasicConfigurator;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import static spark.Spark.get;
 
 public class Main {
     public static void main(String[] args) {
         BasicConfigurator.configure();
-        get("/hello", (req, res) -> "Hello World");
-        get("/", (req, res) -> "Hello");
+        //get("/hello", (req, res) -> "Hello World");
+        get("/", (req, res) -> {
+            return new ModelAndView(null, "index.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
