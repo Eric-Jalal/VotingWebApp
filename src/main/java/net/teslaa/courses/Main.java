890164c0f8046d84631154729996ecfafbe9b581
Amir <Blackworld.2035@gmail.com>
2019-03-29 15:43:52 +0200

    Sign in page

--- src/main/java/net/teslaa/courses/Main.java
+++ src/main/java/net/teslaa/courses/Main.java
@@ -4,7 +4,11 @@ import org.apache.log4j.BasicConfigurator;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,6 +16,12 @@ public class Main {
         //get("/hello", (req, res) -> "Hello World");
         get("/", (req, res) -> {
             return new ModelAndView(null, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/sign-in", (req, res) -> {
+            Map<String, String> mapModel = new HashMap<>();
+            mapModel.put("username", req.queryParams("username"));
+            return new ModelAndView(mapModel, "sign-in.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
