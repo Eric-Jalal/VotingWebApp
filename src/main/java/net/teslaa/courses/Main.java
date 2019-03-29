809efaf578711bec6bb88957aa65bb5c994b722c
Amir <Blackworld.2035@gmail.com>
2019-03-29 15:11:33 +0200

    Added handlebar framework

--- src/main/java/net/teslaa/courses/Main.java
+++ src/main/java/net/teslaa/courses/Main.java
@@ -8,6 +8,6 @@ public class Main {
     public static void main(String[] args) {
         BasicConfigurator.configure();
         get("/hello", (req, res) -> "Hello World");
-
+        get("/", (req, res) -> "Hello");
     }
 }
